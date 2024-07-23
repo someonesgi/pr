8999e8c8fd66dd6377def16b8ece81c81dc98fc6
someonesgi <someoneshotout@hotmail.com>
2024-07-23 09:11:04 -0700

    tutirial

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+    #string
 print("Hello World")
+    #expression
+print('!' * 3)
+    #variables
+price = 20
+    #float
+rating = 3.4
+    #boolean
+is_published = True/False
+print(price)
+    #receive input/function
+name = input('What is your name? ')
+print("Hi " + name)
+#End of Day 1
